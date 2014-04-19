d406cdf420c5d7c62a3b1c2305643b1d1cc68db0
tonybergeron <tonybergeron@gmail.com>
2014-04-19 17:42:52 -0500

    adding call and json data response

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var app = require('express').createServer();
 
-var http = require('http');
+var request = require('request');
 var parser = require('xml2json');
 
 app.get('/', function(req, res) {
@@ -9,37 +9,33 @@ app.get('/', function(req, res) {
 
 //CTA Bus Tracker
 app.get('/cta/bustime/getpredictions', function(req, res) {
-	//key
-	//routes
-	//stops
 
 	//use query for url values
-	var key = req.query.key;
+	var key = 'JbbYsQ54N2ar84ALjN2y4sH9d';
 	var rt = req.query.rt;
 	var stpid = req.query.stpid;
 
 	//The url we want is: 'www.random.org/integers/?num=1&min=1&max=10&col=1&base=10&format=plain&rnd=new'
 	var options = {
-	  host: 'http://www.ctabustracker.com',
-	  path: '/bustime/api/v1/getpredictions?key=' + key + '&rt=' + rt + '&stpid=' + stpid;
+	  uri: 'http://www.ctabustracker.com/bustime/api/v1/getpredictions',
+	  qs: {
+	  	key: key,
+	  	rt: rt,
+	  	stpid: stpid
+	  }
 	};
 
-	callback = function(response) {
-	  var str = '';
-
-	  //another chunk of data has been recieved, so append it to `str`
-	  response.on('data', function (chunk) {
-	    str += chunk;
-	  });
-
-	  //the whole response has been recieved, so we just print it out here
-	  response.on('end', function () {
-	    console.log(str);
-	    parser.toJson(str);
-	  });
-	}
-
-	http.request(options, callback).end();
+	
+	request(options, function (error, response, data) {
+	  if (!error && response.statusCode == 200) {
+	  	var json = parser.toJson(data);
+	    console.log(json);
+	    res.send(json);
+	  } else {
+	    console.log(error);
+	    res.send(error);
+	  }
+	});
 
 });
 
